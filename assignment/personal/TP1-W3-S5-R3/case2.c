26cc525bfb39f04cc846a3478bf157569caa1c75
vilis-iv <sarcastrope@gmail.com>
2023-12-01 01:06:52 +0700

    [TP1-W3-S5-R3] case2: documentation

--- assignment/personal/TP1-W3-S5-R3/case2.c
+++ assignment/personal/TP1-W3-S5-R3/case2.c
@@ -3,6 +3,20 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+/****************************
+* structure of 'trx' to contain transaction data consisting of different data types 
+*****************************/
+struct trx {
+    double fPrice;      //Final Price after discount
+    int month;          //Month of Transaction
+    bool deleted;       //Data deletion distinction
+};
+
+/****************************
+* Function Description: Print Program title at the top line of terminal
+* 
+* @output: print line of program title
+*****************************/
 void header() {
     printf("*******************************\n");
     printf("* Program Penghitung Total    *\n");
@@ -10,6 +24,12 @@ void header() {
     printf("*******************************\n");
 }
 
+/****************************
+* Function Description: Print Program output for each iteration of transaction entered
+*
+* @input: int totalGroceries, double basePrice, double price, String discountTag
+* @output: print line of transaction detail
+*****************************/
 void footer(int totalGroceries, double basePrice, double price, char discountTag[10]) {
     printf("\nOUTPUT: ");
     printf("\nTotal Belanja Bulan Ini: %d", totalGroceries);
@@ -20,12 +40,11 @@ void footer(int totalGroceries, double basePrice, double price, char discountTag
 
 }
 
-struct trx {
-    double fPrice;      //Final Price after discount
-    int month;
-    bool deleted;
-};
-
+/****************************
+* Function Description: creating terminal line prompt to get user's confirmation wether to continue to enter next transaction or stop the program
+*
+* @input: String isEntry
+*****************************/
 void nextEntry(char *isEntry) {
         printf("\ncontinue (Y/n): ");
         fgets(isEntry, 10, stdin);
@@ -37,31 +56,33 @@ int main() {
     * PROGRAM TITLE
     *********************/
     header();
-    /**************************
-     * Variables
-     **************************/
-    struct trx *transactions;
-    transactions = malloc(sizeof(struct trx));
-    char isEntry[] = "Y";
-    double price;
-    int month;
-    int index = 0;
+    
+    struct trx *transactions;                       //initializing struct trx as transactions
+    transactions = malloc(sizeof(struct trx));      //allocating memory for transactions
+    char isEntry[] = "Y";                           //initializing isEntry to check wether to continue or stop the program
+    int index = 0;                                  //index to help iteration inside while loop
 
     /********************************
-    * While Loop to keep user always able to enter data 
-    * unless choosing 'N' or 'n' when choosing "continue (Y/n): "
+    * While Loop for user to be able to continously enter data 
+    * unless choosing 'N' or 'n' to stop entry and close the program
     *********************************/
     while(strcmp(isEntry, "N") != 0 && strcmp(isEntry, "n") != 0) {
-        double price;
-        double basePrice;
-        char discountTag[10];
-        transactions = realloc(transactions, (index + 1) * sizeof(struct trx));
+
+        /********************************
+        * re-allocating memory with the help of index to initialize array's size of transactions 
+        * and to create a dynamic array for transactions when user decided to continue the program
+        *********************************/
+        transactions = realloc(transactions, (index + 1) * sizeof(struct trx));     
+
+        double basePrice;                           //Actual Price
+        double price;                               //Discounted Price
+        char discountTag[10];                       //Discount Tag eg: "20%" or "30% + 20%"
         printf("\nINPUT: ");
         printf("\nBulan Belanja (1-12): ");
-        scanf("%d", &transactions[index].month);
+        scanf ("%d", &transactions[index].month);
         printf("Total Belanja Rp: ");
         scanf ("%lf", &transactions[index].fPrice);
-        getchar();
+        getchar();                                  //getchar() to clean trailing "\n" after scanf in order to not distrupt fgets inside nextEntry function
         
         price = transactions[index].fPrice;
         basePrice = price;
@@ -88,14 +109,18 @@ int main() {
 
         transactions[index].deleted = false;
 
-        int totalGroceriesInMonth = 0;
+        /**************************************
+        * Check for total transaction on given month
+        * totalGroceries will 
+        ***************************************/
+        int totalGroceries = 0;
         for(int i = 0; i < index + 1; i++) {
             if(transactions[index].month == transactions[i].month && !transactions[i].deleted) {
-                totalGroceriesInMonth++;
+                totalGroceries++;
             }
         }
 
-        if(totalGroceriesInMonth == 4) {
+        if(totalGroceries == 4) {
             char extraDiscountTag[] = " + 20%";
             strcat(discountTag, extraDiscountTag);
             price = price - (price * 20 / 100);
@@ -108,7 +133,7 @@ int main() {
         }
 
         index++;
-        footer(totalGroceriesInMonth, basePrice, price, discountTag);
+        footer(totalGroceries, basePrice, price, discountTag);
         nextEntry(isEntry);
     }
 
@@ -118,6 +143,5 @@ int main() {
     //     printf("\n%s", transactions[i].deleted ? "true" : "false");
     // }
 
-    
     return 0;
 }
