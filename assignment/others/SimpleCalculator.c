59239043ca0756bd510336b072d409fead97e1b0
vilis-iv <sarcastrope@gmail.com>
2023-12-03 20:08:28 +0700

    5.2 study case simple calculator

---
+++ assignment/others/SimpleCalculator.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+float calc(char operator, float numX, float numY) {
+    float result = 0;
+    if(operator == '+') result = numX + numY;
+    else if(operator == '-') result = numX - numY;
+    else if(operator == '*') result = numX * numY;
+    else if(operator == '/') result = numX / numY;
+    return result;
+}
+
+int main() {
+    float num1;
+    float num2;
+    float num3;
+    float result;
+    char operator1;
+    char operator2;
+    printf("Equation = ");
+    scanf("%f %c %f %c %f", &num1, &operator1, &num2, &operator2, &num3);
+    
+    result = (operator2 == '*' || operator2 == '/') 
+                ? calc(operator1, num1, calc(operator2, num2, num3))
+                : calc(operator2, num3, calc(operator1, num1, num2));
+
+    printf("\nResult = %.1f", result);
+    return 0;
+}
