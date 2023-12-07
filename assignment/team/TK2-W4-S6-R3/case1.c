2f707cf25393cb69a31424d828851c90580f7588
vilis-iv <sarcastrope@gmail.com>
2023-12-07 12:36:49 +0700

    [TK2-W4-S6-R3] case1

---
+++ assignment/team/TK2-W4-S6-R3/case1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int hours; 
+    double costs = 10000;    
+    printf("Durasi (Jam): ");
+    scanf("%d", &hours);
+
+    costs *= hours;
+    if (hours >= 4 && hours < 6) costs -= costs * 10/100;
+    else if (hours >= 6 && hours < 8) costs -= costs * 15/100;
+    else if (hours == 8) costs -= costs * 20/100;
+    else costs -= costs * 25/100;
+
+    printf("\nLama pemakaian: %d jam", hours);
+    printf("\nBiaya pemakaian: Rp %.2f", costs);
+    return 0;
+}
