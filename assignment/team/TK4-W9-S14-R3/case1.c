78fcc089651153d89d18a7ee411ed9a8e0edb062
vilis-iv <sarcastrope@gmail.com>
2024-01-22 11:12:12 +0700

    add documentation

--- assignment/team/TK4-W9-S14-R3/case1.c
+++ assignment/team/TK4-W9-S14-R3/case1.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+/***********************
+* struct for Book
+************************/
 struct Book {
     int bookId;
     char name[256];
@@ -9,23 +13,30 @@ struct Book {
     char price[256];
 };
 
+
+/***********************
+* function to insert book
+************************/
 void inputBook(struct Book *book, int index) {
     char name[256];
     char category[256];
     char price[256];
     
-    // book = realloc(book, (index + 1) * sizeof(struct Book));     
     
-    book[index].bookId = index;
+    book[index].bookId = index;         //create book id
     printf("Name: ");
-    scanf("%s", book[index].name);
+    scanf("%s", book[index].name);      //input book name
     printf("Categories: ");
-    scanf("%s", book[index].category);
+    scanf("%s", book[index].category);  //input book category
     printf("Price: ");
-    scanf("%s", book[index].price);
+    scanf("%s", book[index].price);     //input book price
 
 }
 
+
+/***********************
+* function to view book data from input
+************************/
 void viewData(struct Book *book, int index) {
     for (int i = 0; i < index; i++) {
         printf("\nindex: %d\n", i + 1);
@@ -35,45 +46,59 @@ void viewData(struct Book *book, int index) {
     } 
 }
 
+/***********************
+* function to delete book data from input
+************************/
 void deleteData(struct Book *book, int index) {
-    viewData(book, index);
+    viewData(book, index);              //view all book
     int id;
     printf("Select Id:");
-    scanf("%d", &id);
+    scanf("%d", &id);                   //select index of book to delete
     
     if (id < 0 || id > index) {
         printf("\nindex can not be found\n");
     } else {
          for(int i=id-1; i<index-1; i++)
         {
-            book[i] = book[i + 1];
+            book[i] = book[i + 1];      //replace selected book with next iteration of book to delete it 
         }
     }
 }
 
+
+/***********************
+* function to save book data
+************************/
 void saveData(struct Book *book, int index) {
     FILE *pFile;
-    pFile = fopen("databuku.txt", "a");
+    pFile = fopen("databuku.txt", "a");                         //append data if file exist
+
+    if (pFile == NULL) pFile = fopen("databuku.txt", "w");      //create data if file does not exist
+    else
     for (int i = 0; i < index; i++) {
 
         char id[10];
-        sprintf(id, "%d", book[i].bookId);
+        sprintf(id, "%d", book[i].bookId);                      //parse string id to integer
         
-        fputs(id, pFile);
+        fputs(id, pFile);                                       //put id into file
         fputs("\n", pFile);
         fputs(book[i].name, pFile);
-        fputs("\n", pFile);
+        fputs("\n", pFile);                                     //put name into file
         fputs(book[i].category, pFile);
-        fputs("\n", pFile);
+        fputs("\n", pFile);                                     //put category into file
         fputs(book[i].price, pFile);
-        fputs("\n", pFile);
+        fputs("\n", pFile);                                     //put price into file
     }
         fclose(pFile);
 }
 
+
+/***********************
+* load data from file into struct of Book
+************************/
 int loadFile(struct Book *book) {
     FILE *pFile;
-    pFile = fopen("databuku.txt", "r");
+    pFile = fopen("databuku.txt", "r");                         //read the file
 
     char bookId[256];
     char name[256];
@@ -85,17 +110,17 @@ int loadFile(struct Book *book) {
     if (pFile == NULL) perror("Failed to open file");
     else {
         while (!feof(pFile)) {
-            fscanf(pFile, "%s", bookId);
-            fscanf(pFile, "%s", name);
-            fscanf(pFile, "%s", category);
-            fscanf(pFile, "%s", price);
+            fscanf(pFile, "%s", bookId);                    //bind data into bookId
+            fscanf(pFile, "%s", name);                      //bind data into name
+            fscanf(pFile, "%s", category);                  //bind data into category
+            fscanf(pFile, "%s", price);                     //bind data into price
             int id;
-            sscanf(bookId, "%d", &id);
+            sscanf(bookId, "%d", &id);                      //parse string id into integer id
 
-            book[index].bookId = id;
-            strcpy(book[index].name, name);
-            strcpy(book[index].category, category);
-            strcpy(book[index].price, price);
+            book[index].bookId = id;                        //set id into struct
+            strcpy(book[index].name, name);                 //set name into struct
+            strcpy(book[index].category, category);         //set category into struct 
+            strcpy(book[index].price, price);               //set price into struct
             
             index++;
         }
@@ -103,25 +128,28 @@ int loadFile(struct Book *book) {
     return index;
 }
 
+/***********************
+* view data from file
+************************/
 void viewFile(struct Book *book, int itemCount) {
     for (int i = 0; i < itemCount - 1; i++) {
         printf("book id: %d\n", book[i].bookId);
-        printf("book id: %s\n", book[i].name);
-        printf("book id: %s\n", book[i].category);
-        printf("book id: %s\n", book[i].price);
+        printf("name: %s\n", book[i].name);
+        printf("category: %s\n", book[i].category);
+        printf("price: %s\n", book[i].price);
     }
 }
 
 int main() {
     int i = 0;
-    int index = 0;
+    int index = 0;                      //count item for array of struct book
     // FILE *in;
-    struct Book *book;
-    struct Book *file;
-    book = malloc(sizeof(struct Book));      
-    file = malloc(sizeof(struct Book));      
+    struct Book *book;                  //struct book to hold input value
+    struct Book *file;                  //struct file to hold file value
+    book = malloc(sizeof(struct Book));      //allocate memory for book
+    file = malloc(sizeof(struct Book));      //allocate memory for file
     
-    while (i != 6) {
+    while (i != 6) {                         //check to stop application when menu 6. exit is selected
     printf("===============BOOK STORE==================\n");
     printf("1. Input\n");
     printf("2. View History\n");
@@ -133,12 +161,12 @@ int main() {
     scanf("%d", &i);
     getchar();
     
-    int itemCount = loadFile(file);
+    int itemCount = loadFile(file);         //count item to define array of struct item size
     
     switch (i) {
         case 1 : printf("Input Book\n");
         inputBook(book, index);
-        index++;
+        index++;                            //increase index for array of book size
         break;
 
         case 2 : printf("View History\n");
@@ -149,11 +177,13 @@ int main() {
         viewFile(file, itemCount);
         break;
 
-        case 4 : printf("delete history");
+        case 4 : printf("delete history\n");
         deleteData(book, index);
-        index--;
+        index--;                            //decrease index for array of book size
         break;
-        case 5 : printf("delete book");
+        case 5 : printf("delete book\n");
+        deleteData(file, itemCount);
+        itemCount--;
         break;
         case 6 : printf("exit\n");
         saveData(file, index);
