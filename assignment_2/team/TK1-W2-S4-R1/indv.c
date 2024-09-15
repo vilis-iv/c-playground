e469b54fecc5b29bf3e72b29891c6843ad5f14d6
vilis-iv <107937882+vilis-iv@users.noreply.github.com>
2024-09-16 02:40:05 +0700

    Update indv.c

--- assignment_2/team/TK1-W2-S4-R1/indv.c
+++ assignment_2/team/TK1-W2-S4-R1/indv.c
@@ -1,12 +1,3 @@
-// Buatlah sebuah program database sederhana dengan menggunakan singly linked list!
-// Program tersebut memiliki pilihan(menu) sebagai berikut:
-// 1.	Push data karyawan. Ketika dijalankan, akan diminta input berupa Employee ID, Nama Lengkap, Tempat Lahir, Tanggal Lahir, dan Jabatan. Validasi Employee ID maksimal 5 angka, Nama Lengkap dan Tempat lahir maksimal 30 karakter, dan Jabatan maksimal 50 karakter. Validasi jumlah data yang dapat diinput adalah maksimal 5 data.
-// 2.	Menampilkan semua daftar Karyawan di database terdiri dari Employee ID, Nama Lengkap, Tempat Tanggal Lahir, dan Jabatan. Data karyawan ditampilkan secara terurut (sort) berdasarkan Employee ID. 
-// 3.	Hapus data karyawan berdasarkan Employee ID
-// 4.	Hapus semua data karyawan
-// 5.	Keluar dari program.
-//  dalam bahasa C 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
